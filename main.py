38d6213142c1a91546e2819411afae449173c9ab
errenxr <vttaengularity@gmail.com>
2026-05-11 03:33:48 +0700

    update q-table

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import tkinter as tk
 from ui.screens.menu_screen import MenuScreen
 from ui.screens.game_screen import GameScreen
+import time
+import requests
+
+def wait_server():
+    while True:
+        try:
+            requests.get(
+                "http://10.193.182.65:5000/api/get_current_user",
+                timeout=3
+            )
+            print("Server connected")
+            break
+
+        except:
+            print("Waiting Flask server...")
+            time.sleep(5)
 
 def start_game(mode):
     for widget in root.winfo_children():
